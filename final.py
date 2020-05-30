0398027d08318ef9e89b593c5d4e91b3524e3818
Jessus911 <jkspidus@gmail.com>
2020-05-30 14:37:55 +0200

    Update final.py

--- final.py
+++ final.py
@@ -1,11 +1,11 @@
-from bts import BTS
+from bst import BST
 from lista import LinkedList
 from random import randint
 import time
 ONE_SECOND = 1000
-REPEATS = 1000
+REPEATS = 10
 
-instances = [10, 50, 100, 200, 500, 1000, 2000, 2500, 3600, 5000]
+instances = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
 
 def fill_increasing(size):
     array = []
@@ -24,55 +24,58 @@ def fill_random(size):
 
 def time_clock(array, structure):
     start_time_append = time.time()
-    for i in array:
-        structure.append(i)
+    for run in range(REPEATS):
+        for i in array:
+            structure.append(i)
     end_time_append = time.time()
-    time_append = round((end_time_append - start_time_append))
+    time_append = round((end_time_append - start_time_append) * ONE_SECOND / REPEATS, 3)
     start_time_find = time.time()
-    for i in array:
-        structure.find(i)
+    for run in range(REPEATS):
+        for i in array:
+            structure.find(i)
     end_time_find = time.time()
-    time_find = round((end_time_find - start_time_find))
-    return {'append': time_append,
-            'find': time_find}
+    time_find = round((end_time_find - start_time_find) * ONE_SECOND / REPEATS, 3)
+    return {
+        'append': time_append,
+        'find': time_find
+        }
 
 def linked_increasing(size):
     linked_list = LinkedList()
     array = fill_increasing(size)
     times = time_clock(array, linked_list)
-    print(f"Linked, append, {size} size, increasing, {times['append']}")
-    print(f"Linked, find, {size} size, increasing, {times['find']}")
+    print(f"Linked, append, {size} size, increasing, {round(times['append'] / size, 5)}")
+    print(f"Linked, find, {size} size, increasing, {round(times['find'] / size, 5)}")
 
 def linked_random(size):
     linked_list = LinkedList()
     array = fill_random(size)
     times = time_clock(array, linked_list)
-    print(f"Linked, append, {size} size, random, {times['append']}")
-    print(f"Linked, find, {size} size, random, {times['find']}")
+    print(f"Linked, append, {size} size, random, {round(times['append'] / size, 5)}")
+    print(f"Linked, find, {size} size, random, {round(times['find'] / size, 5)}")
 
-def bts_increasing(size):
-    bts = BTS()
+def bst_increasing(size):
+    bst = BST()
     array = fill_increasing(size)
-    times = time_clock(array, bts)
-    print(f"BTS, append, {size} size, increasing, {times['append']}")
-    print(f"BTS, find, {size} size, increasing, {times['find']}")
+    times = time_clock(array, bst)
+    print(f"BST, append, {size} size, increasing, {round(times['append'] / size, 5)}")
+    print(f"BST, find, {size} size, increasing, {round(times['find'] / size, 5)}")
 
-def bts_random(size):
-    bts = BTS()
+def bst_random(size):
+    bst = BST()
     array = fill_random(size)
-    times = time_clock(array, bts)
-    print(f"BTS, append, {size} size, random, {times['append']}")
-    print(f"BTS, find, {size} size, random, {times['find']}")
+    times = time_clock(array, bst)
+    print(f"BST, append, {size} size, random, {round(times['append'] / size, 5)}")
+    print(f"BST, find, {size} size, random, {round(times['find'] / size, 5)}")
 
 def main():
     for instance in instances:
         try:
-            size = instance * REPEATS
-            linked_increasing(size)
-            linked_random(size)
-            bts_increasing(size)
-            bts_random(size)
-        except RuntimeError as RE:
-            print(f'Error in {RE}')
+            linked_increasing(instance)
+            linked_random(instance)
+            bst_increasing(instance)
+            bst_random(instance)
+        except RuntimeError as re:
+            print(f'Error in {re}')
 
 main()
